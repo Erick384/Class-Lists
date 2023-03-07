6213e42a149d10843d2a0459a0645c001b181d6c
mwongess <amosmwongelah@gmail.com>
2023-03-07 15:40:28 +0300

    Update classlist

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
 const express = require("express");
 const server = express();
+server.use(express.json())
+const classlist = [
+  { id: 1, name: "Erick" },
+  { id: 2, name: "Mathenge" },
+  { id: 3, name: "Amos" },
+];
+
+server.get("/class", (req, res) => {
+  res.send(classlist);
+});
+
+server.post("/class", (req, res) => {
+  try {
+    classlist.push(req.body)
+    res.send(classlist)
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+server.get("/class/:id", (req, res)=>{
+  const id = req.params.id
+
+
+  const person = classlist.filter(person=>{
+    return person.id == id
+  })
+  res.send(person)
+})
 
 server.listen(4000, () => {
   console.log("Server up and running");
